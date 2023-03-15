8c98ce93a0ec582dd1b744fcad644e41e20fc433
tororo_1066 <panshoku4@gmail.com>
2023-03-15 15:26:26 +0900

    バグ修正

--- src/main/java/tororo1066/identityfifty/character/survivor/Helper.kt
+++ src/main/java/tororo1066/identityfifty/character/survivor/Helper.kt
@@ -29,7 +29,10 @@ class Helper : AbstractSurvivor("helper") {
             .addLore(translate("helper_protect_lore_3"))
 
         val protectSkillItem = IdentityFifty.interactManager.createSInteractItem(protectSkill,true).setInteractEvent { _, _ ->
-            val nearPlayer = p.location.getNearbyPlayers(8.0).filter { IdentityFifty.identityFiftyTask?.aliveSurvivors()?.contains(it.uniqueId) == true && it.uniqueId != p.uniqueId }.firstOrNull()
+            val nearPlayer = p.location.getNearbyPlayers(8.0).firstOrNull {
+                IdentityFifty.identityFiftyTask?.aliveSurvivors()
+                    ?.contains(it.uniqueId) == true && it.uniqueId != p.uniqueId
+            }
             if (nearPlayer == null){
                 p.sendActionBar(Component.text(translate("helper_protect_cant_use")))
                 return@setInteractEvent false
