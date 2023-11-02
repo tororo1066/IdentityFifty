c4bf69e46595455960ee133a1eb1c744e24552fd
DeathNeeet <nagasakaktr524@gmail.com>
2023-11-03 00:23:54 +0900

    一筋の縄、解除から３０秒間牛型発電機の発電速度が30%上昇する効果を追加&説明文変更

--- src/main/java/tororo1066/identityfifty/talent/survivor/GateOpenBoost.kt
+++ src/main/java/tororo1066/identityfifty/talent/survivor/GateOpenBoost.kt
@@ -12,11 +12,15 @@ class GateOpenBoost : AbstractSurvivorTalent("gate_open_boost", 5,FullCowUp::cla
     override fun lore(): List<String> {
         return listOf("gate_open_boost_lore_1")
     }
-
+    var cowbuff = false
     override fun onFinishedGenerator(dieLocation: Location, remainingGenerator: Int, p: Player) {
         if (remainingGenerator == 0){
             val data = IdentityFifty.survivors[p.uniqueId]!!
             data.setHealth(data.getHealth() + 2)
+            cowbuff = true
+            Bukkit.getScheduler().runTaskLater(IdentityFifty.plugin, Runnable {
+                cowbuff = false
+            },600)
             p.addPotionEffect(PotionEffect(PotionEffectType.SPEED, 100, 2))
             val footprints = UUID.randomUUID()
             data.footprintsModify[footprints] = 0.0
@@ -24,6 +28,13 @@ class GateOpenBoost : AbstractSurvivorTalent("gate_open_boost", 5,FullCowUp::cla
                 data.footprintsModify.remove(footprints)
             }, 100)
         }
+    }
+
+    override fun cowGeneratorModify(damage: Double, maxHealth: Double, nowHealth: Double, p: Player): Double {
+        if (cowbuff){
+            return damage * 1.30
+        }
+        return damage
     }
 
 
