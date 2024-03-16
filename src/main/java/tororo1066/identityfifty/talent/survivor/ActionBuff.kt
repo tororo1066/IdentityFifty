cf854a22070b435d0ee327e6df5e965e7af79f83
DeathNeeet <nagasakaktr524@gmail.com>
2024-03-16 15:28:42 +0900

    しゅうせい

---
+++ src/main/java/tororo1066/identityfifty/talent/survivor/ActionBuff.kt
@@ -0,0 +1,76 @@
+package tororo1066.identityfifty.talent.survivor
+
+import org.bukkit.Location
+import org.bukkit.attribute.AttributeModifier
+import org.bukkit.entity.Player
+import org.bukkit.scheduler.BukkitTask
+import tororo1066.identityfifty.IdentityFifty
+
+class ActionBuff : AbstractSurvivorTalent("action_buff", 5, FullSheepUp::class.java) {
+
+    override fun lore(): List<String> {
+        return listOf("action_buff_lore_1","action_buff_lore_2")
+    }
+
+    var task: BukkitTask? = null
+    var actionPoint = 0.0
+
+    fun addActionPoint(point: Double) {
+        if (actionPoint + point > 100) {
+            actionPoint = 100.0
+        }
+        else if(actionPoint + point < 0){
+            actionPoint = 0.0
+        }
+        else {
+            actionPoint += point
+        }
+    }
+
+    override fun onDamage(damage: Int, toHealth: Int, stun: Boolean, damager: Player, p: Player): Pair<Boolean, Int> {
+        addActionPoint(-75.0)
+        return super.onDamage(damage, toHealth, stun, damager, p)
+    }
+
+    override fun onEnterWindow(p: Player) {
+        //actionPoint += 1
+
+        task = IdentityFifty.speedModifier(p, (0.15 * (actionPoint / 100)), Int.MAX_VALUE, AttributeModifier.Operation.ADD_SCALAR)
+    }
+
+    override fun onExitWindow(p: Player) {
+        task?.cancel()
+    }
+
+    override fun sheepGeneratorModify(damage: Double, remainingGenerator: Int, maxHealth: Double, nowHealth: Double, p: Player): Double {
+        addActionPoint(damage*0.03)
+        val buff = 1 + (0.20 *(actionPoint/100))
+        return damage * buff
+    }
+
+    override fun cowGeneratorModify(damage: Double, maxHealth: Double, nowHealth: Double, p: Player): Double {
+        addActionPoint(damage*0.03)
+        val buff = 1 + (0.20 *(actionPoint/100))
+        return damage * buff
+    }
+
+    override fun onHelp(helpedPlayer: Player, p: Player) {
+        addActionPoint(10.0)
+    }
+
+    override fun onGotHelp(helper: Player, p: Player) {
+        addActionPoint(20.0)
+    }
+
+    override fun onWoodPlate(loc: Location, p: Player) {
+        addActionPoint(5.0)
+    }
+
+    override fun onHitWoodPlate(hittedPlayer: Player, loc: Location, blindTime: Int, slowTime: Int, p: Player): Pair<Int, Int> {
+        addActionPoint(15.0)
+        val plateBuff = 1 + (0.20*(actionPoint/100))
+        return Pair((blindTime * plateBuff).toInt(),(slowTime * plateBuff).toInt())
+    }
+
+
+}
---
+++ src/main/java/tororo1066/identityfifty/talent/survivor/DamagedDelay.kt
@@ -0,0 +1,40 @@
+package tororo1066.identityfifty.talent.survivor
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import tororo1066.identityfifty.IdentityFifty
+
+class DamagedDelay : AbstractSurvivorTalent("damaged_delay",5,FullCowUp::class.java) {
+
+    override fun lore(): List<String> {
+        return listOf("damaged_delay_lore_1","damaged_delay_lore_2")
+    }
+
+    //var canfire = false
+
+//    override fun onStart(p: Player) {
+//        tasks.add(Bukkit.getScheduler().runTaskTimer(IdentityFifty.plugin, Runnable {
+//            val suvdata = IdentityFifty.survivors[p.uniqueId]!!
+//            val health = suvdata.getHealth()
+//        },0,5))
+//    }
+
+    var canUse = true
+
+    override fun onDamage(damage: Int, toHealth: Int, stun: Boolean, damager: Player, p: Player): Pair<Boolean, Int> {
+        if (toHealth == 1 && canUse){
+            canUse = false
+            val data = IdentityFifty.survivors[p.uniqueId]!!
+            Bukkit.getScheduler().runTaskLater(IdentityFifty.plugin, Runnable {
+                data.setHealth(data.getHealth() - damage)
+            },400)
+            Bukkit.getScheduler().runTaskLater(IdentityFifty.plugin, Runnable {
+                canUse = true
+            },4800)
+            return Pair(true,0)
+
+
+        }
+        return Pair(true,damage)
+    }
+}
