cffbf83088a77f39691a2deb03eb32477f8da5ed
DeathNeeet <nagasakaktr524@gmail.com>
2024-07-03 00:33:11 +0900

    古傷調整

--- src/main/java/tororo1066/identityfifty/talent/hunter/SurvivorHealedStop.kt
+++ src/main/java/tororo1066/identityfifty/talent/hunter/SurvivorHealedStop.kt
@@ -1,10 +1,12 @@
 package tororo1066.identityfifty.talent.hunter
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import tororo1066.identityfifty.IdentityFifty
 import tororo1066.tororopluginapi.lang.SLang.Companion.sendTranslateMsg
+import java.util.*
 
 class SurvivorHealedStop : AbstractHunterTalent("survivor_healed_stop",2,RemainTimeDown::class.java) {
     override fun lore(): List<String> {
@@ -12,7 +14,12 @@ class SurvivorHealedStop : AbstractHunterTalent("survivor_healed_stop",2,RemainT
     }
 
     override fun onSurvivorHeal(healPlayer: Player, healedPlayer: Player, p: Player) {
-        healedPlayer.addPotionEffect(PotionEffect(PotionEffectType.SLOW,100,200))
-        healedPlayer.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS,140,3))
+        healedPlayer.addPotionEffect(PotionEffect(PotionEffectType.SLOW,140,1))
+        val data = IdentityFifty.survivors[healedPlayer.uniqueId]!!
+        val uuid = UUID.randomUUID()
+        data.footprintsModify += uuid to 2.0
+        Bukkit.getScheduler().runTaskLater(IdentityFifty.plugin, Runnable {
+            data.footprintsModify -= uuid
+        }, 200)
     }
 }
